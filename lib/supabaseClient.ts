348d3a1b1990c0e4f4de8a042a1ffd0774824280
Anuja Jayasinghe <anujajayasinhe@gmail.com>
2025-06-22 03:11:17 +0530

    npm fix

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = "https://utknvryduzgxcsqapkmi.supabase.co";
+const supabaseAnonKey ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0a252cnlkdXpneGNzcWFwa21pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAwMzk2OTIsImV4cCI6MjA2NTYxNTY5Mn0.XdbL34EIDOsPT54uuIczu-kTDXqwMoy-JpS-O6d3URw";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,9 @@
 import { DollarSign, Calendar, Target, TrendingUp, ArrowRight, Shield, Sparkles, ChevronDown } from "lucide-react"
 import Link from "next/link"
 import { useEffect, useState } from "react"
-import Header from "@/components/Header"
+import Header from "@/components/Navigation"
 import Footer from "@/components/Footer"
 import Head from "next/head"
-import Image from "next/image"
 
 export default function Home() {
   const [isLoaded, setIsLoaded] = useState(false)
